b070c2d5cb217c84941f48ce8a6785ca8eee7ed8
eelcoverbrugge <hallo@eelcoverbrugge.nl>
2021-12-11 14:17:29 +0100

    Service Objects

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     /**
-     * @Route("/comments/{id}/vote/{direction<up|down>}", name="comments_id_vote_direction")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote(int $id, string $direction): Response
+    public function commentVote(int $id, string $direction, LoggerInterface $logger): Response
     {
 
         if ($direction === 'up') {
             $currentVoteCount = rand(7, 100);
+            $logger->info('Voting up');
         } else {
             $currentVoteCount = rand(0, 5);
+            $logger->info('Voting down');
         }
 
         return $this->json([
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
     /**
      * @Route("/homepage", name="homepage")
      */
-    public function homepage(): Response
+    public function homepage(Environment $twigEnvironment): Response
     {
+        /* Fun example of using the Twig service directly 
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+        */
+
         return $this->render('question/homepage.html.twig');
     }
 
@@ -26,8 +33,6 @@ class QuestionController extends AbstractController
             'Honestly, I like furry shoes better then MY cat',
             'Maybe... try sating the spell backwards?',
         ];
-
-        dump($answers);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
